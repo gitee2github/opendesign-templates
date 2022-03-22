0a749a89fac8de63933579e6e514517dd65241d7
devin-cwd <caowendong@huawei.com>
2022-03-22 11:09:45 +0800

    init

---
+++ packages/vue3-ts-monorepo-starter/packages/demo1/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AppHeader from './components/AppHeader.vue';
+</script>
+
+<template>
+  <AppHeader />
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
